528300e59d16c2080daefbb2084305a1321c14b1
Variegatedhuman <116999437+Variegatedhuman@users.noreply.github.com>
2023-01-11 01:49:08 -0600

    finished product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let weather =  {
      apiKey: "c1997b3305d29a5d4309def698b8fa53",
-     fetchWeather: function (city) {
+     searchWeather: function (city) {
         fetch(
         "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + this.apiKey)
         .then((response) => response.json())
@@ -111,14 +111,18 @@ let weather =  {
       document.querySelector(".humidity-6").innerText = "Humidity: " + humidity + "%";
       document.querySelector(".wind-6").innerText = "Wind speed: " + speed + " MPH";
    },
+
+   
      search: function () {
-        this.fetchWeather(document.querySelector(".searchBar").value);
+        this.searchWeather(document.querySelector(".searchBar").value);
         recentCity();
+        
      }
 };
 
 document.querySelector(".searchBtn").addEventListener("click", function () {
     weather.search();
+    
    
 
 })
@@ -133,7 +137,7 @@ document.querySelector(".searchBar").addEventListener("keyup", function (event) 
 
 })
 
-weather.fetchWeather("Chicago")
+weather.searchWeather("Chicago")
 
 
 function recentCity () {
@@ -176,208 +180,29 @@ function recentCity () {
         
                     let createLi = document.createElement("button");
                     createLi.textContent = allCities[i].userSearch;
-                    let contentEL = document.querySelector(".content")
-                    contentEL.appendChild(createLi);;
-        
+                    let contentEl = document.querySelector(".content")
+                    contentEl.appendChild(createLi);;
+                    createLi.classList.add("listedCity")
+                    
+                    
                 }
+                
             }
+            document.querySelector(".content").addEventListener("click", function (event) {
+                var element = event.target;
+                if (element.matches("button")) {
+                    var searchedCityName = element.textContent;
+                    weather.searchWeather(searchedCityName);
+
+                    
+                    
+
+                    
+            
+                }
+            });
 
 
 }
 
 
-// function recentCity () {
-//    const recentCitySearch = {
-//          recentCitySearch: weather.search,
-//    }
-//    localStorage.setItem("recentCity", JSON.stringify(recentCity));
-
-//    let recentCities = JSON.parse(localStorage.getItem("recentCity"));
-//    if (recentCities !== null) {
-//       document.getElementsByClassName(".content").innerHTML = recentCitySearch;
-//   } else {
-//     return;
-//   }
-//       let createLi = document.createElement("li");
-//       createLi.textContent = recentCitySearch
-//       recentCitySearch.push(recentCities)
-//       console.log ()
-
-//    let contentEL = document.querySelector(".content")
-//       contentEL.appendChild(createLi);
-// }
-
-// function rejuvinateRecentCities () {
-//       let recentCities = JSON.parse(localStorage.getItem("recentCity"));
-//       if (recentCities !== null) {
-//          document.getElementsByClassName(".content").innerHTML = recentCitySearch;
-//      } else {
-//        return;
-//      }
-//          let createLi = document.createElement("li");
-//          createLi.textContent = recentCitySearch
-//          recentCitySearch.push(recentCities)
-//          console.log (recentCities)
-//       }
-
-
-
-// let createLi = document.createElement("li")
-//             createLi.textContent = allScores[i].userData + " scored " + allScores[i].score + " point(s)";
-//             highScore.appendChild(createLi);
-               //         let currentScore = {
-               //             userData: userData,
-               //             score: score,
-               //         }
-               //         console.log(currentScore);
-               //         let allScores = localStorage.getItem("allScores");
-               //         if (allScores === null) {
-               //             allScores = [];
-               //         } else {
-               //             allScores = JSON.parse(allScores);
-               //         }
-               //         allScores.push(currentScore);
-               //         let newScore = JSON.stringify(allScores);
-               //         localStorage.setItem("allScores", newScore);
-               //         openLeaderBoard();
-               //     }
-               
-               
-               //     let allScores = localStorage.getItem("allScores");
-               //     allScores = JSON.parse(allScores);
-               
-               //     if (allScores !== null) {
-               
-               //         for (let i = 0; i < allScores.length; i++) {
-               
-               //             let createLi = document.createElement("li");
-               //             createLi.textContent = allScores[i].userData + " scored " + allScores[i].score + " point(s)";
-               //             highScore.appendChild(createLi);
-
-
-
-// //   function renderLastScore() {
-// //     let lastScore = JSON.parse(localStorage.getItem("userScore"));
-// //     if (lastScore !== null) {
-// //     document.getElementsByClassName("user-Score").innerHTML = lastScore.user;
-// //     document.getElementsByClassName("user-Score").innerHTML = lastScore.currentScore;
-// //     } else {
-// //       return;
-// //     }
-// //   }
-
-
-// startButton.addEventListener("click", startQuiz)
-// restartButton.addEventListener("click", function () {
-//     window.location.replace("./index.html");
-
-// });
-
-// initialButton.addEventListener("click", function () {
-
-
-//     let userData = initials.value;
-
-//     if (userData == "" || userData.length == 0 || userData == null) {
-
-//         alert("Please enter initials");
-//         return
-
-//     } else {
-//         let currentScore = {
-//             userData: userData,
-//             score: score,
-//         }
-//         console.log(currentScore);
-//         let allScores = localStorage.getItem("allScores");
-//         if (allScores === null) {
-//             allScores = [];
-//         } else {
-//             allScores = JSON.parse(allScores);
-//         }
-//         allScores.push(currentScore);
-//         let newScore = JSON.stringify(allScores);
-//         localStorage.setItem("allScores", newScore);
-//         openLeaderBoard();
-//     }
-
-
-//     let allScores = localStorage.getItem("allScores");
-//     allScores = JSON.parse(allScores);
-
-//     if (allScores !== null) {
-
-//         for (let i = 0; i < allScores.length; i++) {
-
-//             let createLi = document.createElement("li");
-//             createLi.textContent = allScores[i].userData + " scored " + allScores[i].score + " point(s)";
-//             highScore.appendChild(createLi);
-
-
-// function saveUserScore() {
-//     let userScore = {
-//       user: initials.value,
-//       currentScore: score.value,
-//     };
-//     localStorage.setItem("userScore", JSON.stringify(userScore));
-//   }
-
-//   function renderLastScore() {
-//     let lastScore = JSON.parse(localStorage.getItem("userScore"));
-//     if (lastScore !== null) {
-//     document.getElementsByClassName("user-Score").innerHTML = lastScore.user;
-//     document.getElementsByClassName("user-Score").innerHTML = lastScore.currentScore;
-//     } else {
-//       return;
-//     }
-//   }
-
-
-// startButton.addEventListener("click", startQuiz)
-// restartButton.addEventListener("click", function () {
-//     window.location.replace("./index.html");
-
-// });
-
-// initialButton.addEventListener("click", function () {
-
-
-//     let userSearch = document.querySelector(".searchBar");
-
-//     if (userSearch == "" || userData.length == 0 || userData == null) {
-
-//         alert("Please try a different city");
-//         return
-
-//     } else {
-//         let currentCity = {
-//             userSearch: userSearch,
-//         }
-//         console.log(currentCity);
-//         let allCities = localStorage.getItem("allCities");
-//         if (allCities === null) {
-//             allCities = [];
-//         } else {
-//             allCities = JSON.parse(allCities);
-//         }
-//         allCities.push(currentCity);
-//         let newCity = JSON.stringify(allCities);
-//         localStorage.setItem("allCities", newCity);
-//     }
-
-
-//     let allCities = localStorage.getItem("allCities");
-//     allCities = JSON.parse(allCities);
-
-//     if (allCities !== null) {
-
-//         for (let i = 0; i < allCities.length; i++) {
-
-//             let createLi = document.createElement("li");
-//             createLi.textContent = allCities[i].userSearch;
-//             let contentEL = document.querySelector(".content")
-//             contentEL.appendChild(createLi);;
-
-//         }
-//     }
-// });
